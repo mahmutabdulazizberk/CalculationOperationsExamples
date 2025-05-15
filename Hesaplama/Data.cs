976700804464f47e124b2f8194f148137f143598
Mahmut Abdulaziz BERK <m.abdulazizberk@icloud.com>
2025-05-16 00:36:12 +0300

    Project complated

---
+++ Hesaplama/Data.cs
@@ -0,0 +1,76 @@
+﻿namespace Hesaplama;
+
+public class Data
+{
+    
+    public HesaplamaSonucu Hesapla(int Sayi1, int Sayi2)
+    {
+        HesaplamaSonucu hesaplama = new HesaplamaSonucu();
+        hesaplama.Toplama = Sayi1 + Sayi2;
+        hesaplama.Carpım = Sayi1 * Sayi2;
+        hesaplama.Bolum = Sayi1 / Sayi2;
+        hesaplama.Fark = Sayi1 - Sayi2;
+        return hesaplama;
+    }
+
+    public HesaplamaSonucu Hesapla(Sayılar sayılar)
+    {
+        HesaplamaSonucu hesaplamaSonucu = new HesaplamaSonucu();
+        hesaplamaSonucu.Toplama = sayılar.Sayı1 + sayılar.Sayı2;
+        hesaplamaSonucu.Fark = sayılar.Sayı1 - sayılar.Sayı2;
+        hesaplamaSonucu.Carpım = sayılar.Sayı1 * sayılar.Sayı2;
+        hesaplamaSonucu.Bolum = sayılar.Sayı1 / sayılar.Sayı2;
+        return hesaplamaSonucu;
+    }
+    
+    public List<UsluToplamCarpimSonucu> Hesapla(Rakamlar rakamlar)
+    {
+        List<UsluToplamCarpimSonucu> liste = new List<UsluToplamCarpimSonucu>();
+        HesaplamaSonucu hesaplamaSonucu = new HesaplamaSonucu();
+        for (int i = 1; i <= rakamlar.Us; i++)
+        {
+            int toplam = UsAl(rakamlar.sayi1, i) + UsAl(rakamlar.sayi2, i);
+            int carpim = UsAl(rakamlar.sayi1, i) * UsAl(rakamlar.sayi2, i);
+            liste.Add(new UsluToplamCarpimSonucu() { Toplam = toplam, Carpim = carpim });
+        }
+        return liste;
+    }
+
+    public int UsAl(int taban, int us)
+    {
+        int sonuc=1;
+        for (int i = 1; i <= us; i++)
+        {
+            sonuc = sonuc * taban;
+        }
+        return sonuc;
+    }
+
+    
+
+    public class UsluToplamCarpimSonucu
+    {
+        public int Toplam { get; set; }
+        public int Carpim { get; set; }
+    }
+
+    public class HesaplamaSonucu
+    {
+        public int Toplama { get; set; }
+        public int Carpım { get; set; }
+        public int Bolum { get; set; }
+        public int Fark { get; set; }
+    }
+    public class Sayılar
+    {
+        public int Sayı1 { get; set; }
+        public int Sayı2 { get; set; }
+    }
+
+    public class Rakamlar
+    {
+        public int sayi1 { get; set; } //3
+        public int sayi2 { get; set; } //1 
+        public int Us { get; set; } //2
+    }
+}
---
+++ Hesaplama/Program.cs
@@ -0,0 +1,63 @@
+﻿using Hesaplama;
+using static Hesaplama.Data;
+
+//BirinciOrnegiUygula();
+//IkinciOrnegiUygula();
+UcuncuOrnegiUygula();
+
+void BirinciOrnegiUygula()
+{
+    Data birinciOrnek = new Data();
+    Console.WriteLine("1. Sayıyı Giriniz");
+    int Sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("2. Sayıyı Giriniz");
+    int Sayi2 = Convert.ToInt32(Console.ReadLine());
+
+
+    HesaplamaSonucu sonuc = birinciOrnek.Hesapla(Sayi1, Sayi2);
+    Console.WriteLine("Sonuç;");
+    Console.WriteLine("\nToplama: " + sonuc.Toplama.ToString() +
+                      "\nFark: " + sonuc.Fark.ToString() +
+                      "\nÇarpım: " + sonuc.Carpım.ToString() +
+                      "\nBölüm: " + sonuc.Bolum.ToString());
+
+}
+void IkinciOrnegiUygula()
+{
+    Data birinciOrnek = new Data();
+    Sayılar sayılar = new Sayılar();
+    Console.WriteLine("1. Sayıyı Giriniz: ");
+    sayılar.Sayı1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("2. Sayıyı Giriniz: ");
+    sayılar.Sayı2 = Convert.ToInt32(Console.ReadLine());
+
+    HesaplamaSonucu sonuc = birinciOrnek.Hesapla(sayılar);
+    Console.WriteLine("Sonuç;");
+    Console.WriteLine("Toplam Sonucu: " + sonuc.Toplama.ToString() +
+                      "\nÇıkartma Sonucu: " + sonuc.Fark.ToString() +
+                      "\nÇarpım Sonucu: " + sonuc.Carpım.ToString() +
+                      "\nBölüm Sonucu: " + sonuc.Bolum.ToString());
+}
+void UcuncuOrnegiUygula()
+{
+    Rakamlar rakamlar = new Rakamlar();
+    Console.WriteLine("1. Sayıyı Giriniz!");
+    rakamlar.sayi1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("2. Sayıyı Giriniz!");
+    rakamlar.sayi2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Üssü Giriniz!");
+    rakamlar.Us = Convert.ToInt32(Console.ReadLine());
+
+    Data birinciOrnek = new Data();
+
+    List<UsluToplamCarpimSonucu> result = birinciOrnek.Hesapla(rakamlar);
+
+    foreach (var deger in result)
+    {
+        Console.WriteLine("Toplam: " + deger.Toplam.ToString() + "\nÇarpım: " + deger.Carpim.ToString());
+    }
+}
+
+
+
